26edf78513b6458350593d21093aaf23675b1e56
just-software-dev <idontgiveafuck@hotmail.com>
2025-01-11 02:24:51 -0300

    it's mine now

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["Checkpoints"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/just-software-dev/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         .target(
--- README.md
+++ README.md
@@ -15,7 +15,7 @@ the `dependencies` value of your `Package.swift`.
 
 ```swift
 dependencies: [
-    .package(url: "https://github.com/horizontalsystems/Checkpoints.git", .upToNextMajor(from: "1.0.0"))
+    .package(url: "https://github.com/just-software-dev/Checkpoints.git", .upToNextMajor(from: "1.0.0"))
 ]
 ```
 ## Usage
